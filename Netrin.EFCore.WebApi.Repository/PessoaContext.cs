08d94438c544f28f9ac2799c55aa3c5afa5f3961
carloshrosa90 <carloshrosa90@hotmail.com>
2024-10-07 11:29:12 -0300

    Melhorias e exclusão de projetos desnecessários

--- Netrin.EFCore.WebApi.Repository/PessoaContext.cs
+++ Netrin.EFCore.WebApi.Repository/PessoaContext.cs
@@ -8,20 +8,10 @@ namespace Netrin.EFCore.WebApi
     // CodeFist: Banco será criado a partir do contexto
     public class PessoaContext : DbContext
     {
-        //public PessoaContext()
-        //{
-                
-        //}
-
         public PessoaContext(DbContextOptions<PessoaContext> options) : base(options){}
 
         public DbSet<Pessoa> Pessoas { get; set; }
         public DbSet<Endereco> Enderecos { get; set; }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;User ID=Carlos-Pc\\Carlos;Initial Catalog=master;Data Source=CARLOS-PC\\SQLEXPRESS");
-        //}
     }
 
 }
--- Netrin.teste/Data/cadastro/pessoa.cs
+++ Netrin.teste/Data/cadastro/pessoa.cs
@@ -4,15 +4,15 @@ namespace Netrin.teste.Data.cadastro
 {
 	public class pessoa
 	{
-		public int? int_id { get; set; } = 0;
+		public int id { get; set; }
 		[Required(ErrorMessage = "Nome é obrigatório")]
 		public string? str_nome { get; set; }
 		//[Required(ErrorMessage = "Data de nascimento é obrigatório")]
         //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime? dat_nascimento { get; set; }
 		[Required(ErrorMessage = "CPF é obrigatório")]
-		[MinLength(length:11,ErrorMessage = "Quantidade mínima 8 caracteres")]
-        [MaxLength(length: 11, ErrorMessage = "Quantidade mínima 8 caracteres")]
+		[MinLength(length:11,ErrorMessage = "Quantidade mínima 11 caracteres")]
+        [MaxLength(length: 11, ErrorMessage = "Quantidade mínima 11 caracteres")]
         public string? str_cpf { get; set; }
 		public long? lon_telefone { get; set; }
 		public List<endereco>? endereco { get; set; } = null;
